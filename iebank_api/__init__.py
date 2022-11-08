6563beaae988caebc8d30a08aa8b22460c8ac6a2
martagdvalle <martagdvalle@outlook.es>
2022-11-08 16:56:14 +0100

    third

--- iebank_api/__init__.py
+++ iebank_api/__init__.py
@@ -2,7 +2,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:rmdic1cr7@localhost/iebank'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:CRC175ihe.@localhost:5432/IEbank'
 db = SQLAlchemy(app)
 
 from iebank_api.models import Account
